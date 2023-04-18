9f2813ede34a390c0caa58e9d51c841183c535d3
everodriguezval <eve.rodriguezval@gmail.com>
2023-04-18 23:44:23 +0100

    practicing useState with objects

--- app/src/01-useState/useState-object.jsx
+++ app/src/01-useState/useState-object.jsx
@@ -1,7 +1,27 @@
 import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  // Initialize state value as an object which contains three values
+  const [person, setPerson] = useState({
+    name: 'Peter',
+    age: 33,
+    hobby: 'playing chess'
+  });
+
+
+  const handleClick = () => {
+    // If we just want to update only the person's name we use the spread operator.
+    setPerson({...person, name: 'Michael'});
+  }
+
+  return (
+    <div>
+      <h1>{person.name}</h1>
+      <h2>{person.age}</h2>
+      <h2>Enjoys {person.hobby}</h2>
+      <button className="btn" onClick={handleClick}>Change me</button>
+    </div>
+  )
 };
 
 export default UseStateObject;
--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 
 // import UseStateBasics from './01-useState/useState-basics'
-import UseStateArray from './01-useState/useState-array.jsx'
+// import UseStateArray from './01-useState/useState-array.jsx'
+import UseStateObject from './01-useState/useState-object'
 
 function App() {
   return (
     <div className="container">
-      <UseStateArray />
+      <UseStateObject />
+     {/* <UseStateArray /> */}
      {/* <UseStateBasics /> */}
     </div>
   )
