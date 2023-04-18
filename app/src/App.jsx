8652f80f582dcbb51e18ac271d0da5010c221210
everodriguezval <eve.rodriguezval@gmail.com>
2023-04-19 00:26:49 +0100

    practicing useEffect basics

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 
 // import UseStateBasics from './01-useState/useState-basics'
 // import UseStateArray from './01-useState/useState-array.jsx'
-import UseStateObject from './01-useState/useState-object'
+// import UseStateObject from './01-useState/useState-object'
+import UseEffectBasics from './02-useEffect/useEffect-basics'
 
 function App() {
   return (
     <div className="container">
-      <UseStateObject />
+     <UseEffectBasics />
+     {/* <UseStateObject /> */}
      {/* <UseStateArray /> */}
      {/* <UseStateBasics /> */}
     </div>
